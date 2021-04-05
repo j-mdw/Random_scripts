8c4bd9ebe976a0e2eb3c6c27066c41552169a339
jmaydew <jmaydew@student.42.fr>
2021-04-05 22:03:54 +0200

    +ispwned script

---
+++ ispwned.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+## Has your password been pwned? Find out the secure way
+
+## How to us?
+## bash ispwned.sh <password>
+## Requirements: shasum, curl, bash
+
+HASH=$(echo -n $1 | shasum)
+
+hash_5="${HASH:0:5}"
+hash_35="${HASH:5:35}"
+
+if ! ping -c1 api.pwnedpasswords.com &>/dev/null
+then
+	echo connection to pwnedpasswords failed, please try again later
+	exit 1
+fi
+
+pwned=$(curl -s https://api.pwnedpasswords.com/range/$hash_5 | grep -i $hash_35)
+
+if [ -z $pwned ]
+then
+	echo "You're safe"
+else
+	echo $pwned
+	echo "You've been pwned, change password immediatly"
+fi
